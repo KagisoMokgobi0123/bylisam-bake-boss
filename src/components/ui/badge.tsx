b35c7f7b892434312c47c044c3829d83e73141b9
KagisoMokgobi0123 <162275760+KagisoMokgobi0123@users.noreply.github.com>
2026-07-31 00:13:09 +0200

    Update badge.tsx

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -26,6 +26,12 @@ export interface BadgeProps
   extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof badgeVariants> {}
 
 function Badge({ className, variant, ...props }: BadgeProps) {
+  return (
+    <div
+      className={cn(badgeVariants({ variant }), className)}
+      {...props}
+    />
+  );
 }
 
 export { Badge, badgeVariants };
